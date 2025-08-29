7fff55fc8280814718643d389b1c4c8e12a160bd
204must <mohammadmustjab@gmail.com>
2025-08-29 15:13:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome import Chromedriver
+def open_browser(url):
+    driver = webdriver.Chrome()  # Make sure ChromeDriver is in your PATH
+    driver.get(url)
+    driver.maximize_window()
+    return driver
 
+if __name__ == "__main__":
+    driver = open_browser("https://www.google.com")
+    driver.quit()
